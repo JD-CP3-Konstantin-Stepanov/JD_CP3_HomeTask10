0ff91c08b32fcaa76a417209be9a0daa938ac743
Konstantin <kain7984@gmail.com>
2022-11-06 16:38:51 +0800

    Initial commit.

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("Jack", "Connor", "Harry", "George", "Samuel", "John");
+        List<String> families = Arrays.asList("Evans", "Young", "Harris", "Wilson", "Davies", "Adamson", "Brown");
+        Collection<Person> persons = new ArrayList<>();
+        for (int i = 0; i < 10_000_000L; i++) {
+            persons.add(new Person(
+                    names.get(new Random().nextInt(names.size())),
+                    families.get(new Random().nextInt(families.size())),
+                    new Random().nextInt(100),
+                    Sex.values()[new Random().nextInt(Sex.values().length)],
+                    Education.values()[new Random().nextInt(Education.values().length)])
+            );
+        }
+        //Количество несовершеннолетних
+        long minorCount = persons.stream()
+                .filter(x -> x.getAge() < 18)
+                .count();
+        System.out.println("Minors found: " + minorCount);
+        //Список фамилий призывников
+        List<String> conscriptsList = persons.stream()
+                .filter(x -> x.getAge() >= 18 && x.getAge() <= 27 && x.getSex() == Sex.MAN)
+                .map(Person::getFamily).toList();
+        System.out.println("Conscript man list with surname: " + conscriptsList);
+        //Отсортированный по фамилии список потенциально работоспособных людей с высшим образованием
+        List<String> workClassList = persons.stream()
+                .filter(x -> (x.getEducation() == Education.HIGHER))
+                .filter(y -> (y.getAge() >= 18 && ((y.getSex() == Sex.MAN && y.getAge() <= 65)
+                        || (y.getSex() == Sex.WOMAN && y.getAge() <= 60))))
+                .sorted(Comparator.comparing(Person::getFamily))
+                .map(Person::getFamily).toList();
+        System.out.println("List of alphabetically sorted surname working class: " + workClassList);
+    }
+}
